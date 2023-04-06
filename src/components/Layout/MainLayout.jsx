b9d1b53ebb8e313c501182e72192ae80b87afa3e
Mariela <1038mariela.alvarez@gmail.com>
2023-04-05 21:55:31 -0400

    refactor(app): update according to latest routing. PS-40

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material'
+import NavBar from '../common/NavBar'
 
 const MainLayout = ({ children }) => {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      flexDirection="column"
-      justifyContent="center"
-    >
-      {children}
-      <Outlet />
-    </Box>
+    <>
+      <NavBar />
+      <Box
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+        marginTop="100px"
+      >
+        {children}
+        <Outlet />
+      </Box>
+    </>
   )
 }
 
---
+++ src/routes/common/RegisterUser.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import {
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+  Stack,
+} from '@mui/material'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import FormGroup from '@mui/material/FormGroup'
+import Layout from '../../components/Layout/Layout'
+
+const RegisterUser = () => {
+  return (
+    <Layout title="Registrar Usuario">
+      {' '}
+      <Card
+        sx={{
+          p: 10,
+          py: 5,
+          maxWidth: '50px auto',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 4,
+          borderRadius: '15px',
+        }}
+      >
+        <CardContent sx={{ m: 0 }}>
+          <Typography gutterBottom variant="h4" component="div" sx={{ m: 0 }}>
+            Sign up
+          </Typography>
+        </CardContent>
+        <TextField
+          id="outlined-basic"
+          label="nombre y apellido"
+          variant="outlined"
+          type={'text'}
+        />
+        <TextField
+          id="outlined-basic"
+          label="CI"
+          variant="outlined"
+          type={'text'}
+        />
+        <TextField
+          id="outlined-basic"
+          label="email"
+          variant="outlined"
+          type={'email'}
+        />
+        <TextField
+          id="outlined-basic"
+          label="contraseña"
+          variant="outlined"
+          type={'password'}
+        />
+        <TextField
+          id="outlined-basic"
+          label="confirmar contraseña"
+          variant="outlined"
+          type={'password'}
+        />
+        <Typography gutterBottom variant="h8" component="div" sx={{ m: 0 }}>
+          Recibir notificaciones por:
+        </Typography>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="email"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Whatsapp"
+          />
+        </FormGroup>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained">Cancelar</Button>
+          <Button variant="contained">Registrarse</Button>
+        </Stack>
+      </Card>
+    </Layout>
+  )
+}
+
+export default RegisterUser
