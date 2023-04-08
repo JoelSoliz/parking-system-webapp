ff3d826450572702f732d1f59e5a6cd8d0bcd4a6
Silvia Martinez <201704856Q04856@est.umss.edu>
2023-04-08 10:21:59 -0400

    feat(app): validacion de campos. PS-37

--- src/routes/common/RegisterUser.jsx
+++ src/routes/common/RegisterUser.jsx
@@ -1,3 +1,4 @@
+import { Controller, useForm } from 'react-hook-form'
 import React from 'react'
 import {
   Button,
@@ -11,8 +12,26 @@ import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
 import FormGroup from '@mui/material/FormGroup'
 import Layout from '../../components/Layout/Layout'
+import { RadioButtonChecked } from '@mui/icons-material'
 
+const ERROR_MESSAGES = {
+  maxLength: 'La longitud máxima es',
+  minLength: 'La longitud mínima es',
+  required: 'Este campo es requerido.',
+  numbers: 'Solo se admiten números.',
+  valido: 'Ingrese un número de CI válido.',
+  quantityMin: 'Ingrese 7 dígitos.',
+  quantityMax: 'La cantidad máxima es de 7 dígitos.',
+  email: 'Correo electrónico inválido.',
+  letters: 'Solo se admiten letras.',
+}
 const RegisterUser = () => {
+  const {
+    control,
+    formState: { errors, isValid },
+    handleSubmit,
+    watch,
+  } = useForm({ mode: 'onChange' })
   return (
     <Layout title="Registrar Usuario">
       {' '}
@@ -32,48 +51,188 @@ const RegisterUser = () => {
             Sign up
           </Typography>
         </CardContent>
-        <TextField
-          id="outlined-basic"
-          label="nombre y apellido"
-          variant="outlined"
-          type={'text'}
+        <Controller
+          control={control}
+          name="name"
+          rules={{
+            pattern: {
+              message: ERROR_MESSAGES.letters,
+              value: /^[a-zA-Z\s]*$/i,
+            },
+            maxLength: {
+              message: `${ERROR_MESSAGES.maxLength} 30.`,
+              value: 30,
+            },
+            minLength: { message: `${ERROR_MESSAGES.minLength} 3.`, value: 3 },
+            required: { message: ERROR_MESSAGES.required, value: true },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <>
+              <TextField
+                error={!!errors.name?.message}
+                value={value}
+                onChange={(event) => onChange(event.target.value)}
+                label="Nombre(s)"
+                variant="outlined"
+                type={'text'}
+                helperText={errors.name?.message}
+              />
+            </>
+          )}
         />
-        <TextField
-          id="outlined-basic"
-          label="CI"
-          variant="outlined"
-          type={'text'}
+        <Controller
+          control={control}
+          name="surname"
+          rules={{
+            pattern: {
+              message: ERROR_MESSAGES.letters,
+              value: /^[a-zA-Z\s]*$/i,
+            },
+            maxLength: {
+              message: `${ERROR_MESSAGES.maxLength} 30.`,
+              value: 30,
+            },
+            minLength: { message: `${ERROR_MESSAGES.minLength} 3.`, value: 3 },
+            required: { message: ERROR_MESSAGES.required, value: true },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <>
+              <TextField
+                error={!!errors.surname?.message}
+                value={value}
+                onChange={(event) => onChange(event.target.value)}
+                label="Apellido(s)"
+                variant="outlined"
+                type={'text'}
+                helperText={errors.surname?.message}
+              />
+            </>
+          )}
         />
-        <TextField
-          id="outlined-basic"
-          label="email"
-          variant="outlined"
-          type={'email'}
+        <Controller
+          control={control}
+          name="ci"
+          rules={{
+            required: { message: ERROR_MESSAGES.required, value: true },
+            validate: (value) => {
+              if (isNaN(parseInt(value))) {
+                return ERROR_MESSAGES.valido
+              }
+              if (parseFloat(value) % 1 != 0) {
+                return ERROR_MESSAGES.valido
+              }
+              if (parseInt(value) < 0) {
+                return ERROR_MESSAGES.valido
+              }
+              if (parseInt(value) <= 1000000) {
+                return ERROR_MESSAGES.quantityMin
+              }
+              if (parseInt(value) > 10000000) {
+                return ERROR_MESSAGES.quantityMax
+              }
+              return true
+            },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <>
+              <TextField
+                error={!!errors.ci?.message}
+                value={value}
+                onChange={(event) => onChange(event.target.value)}
+                label="CI"
+                variant="outlined"
+                type={'number'}
+                helperText={errors.ci?.message}
+              />
+            </>
+          )}
         />
-        <TextField
-          id="outlined-basic"
-          label="contraseña"
-          variant="outlined"
-          type={'password'}
+
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            maxLength: {
+              message: `${ERROR_MESSAGES.maxLength} 30.`,
+              value: 30,
+            },
+            minLength: { message: `${ERROR_MESSAGES.minLength} 6.`, value: 6 },
+            pattern: {
+              message: ERROR_MESSAGES.email,
+              value: /^[A-Z0-9._]+@[A-Z0-9.]+\.[A-Z]{2,}$/i,
+            },
+            required: { message: ERROR_MESSAGES.required, value: true },
+          }}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <>
+              <TextField
+                error={!!errors.email?.message}
+                value={value}
+                onChange={(event) => onChange(event.target.value)}
+                label="Correo electrónico"
+                variant="outlined"
+                type={'emailAddress'}
+                helperText={errors.email?.message}
+              />
+            </>
+          )}
         />
-        <TextField
-          id="outlined-basic"
-          label="confirmar contraseña"
-          variant="outlined"
-          type={'password'}
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            maxLength: { message: `${ERROR_MESSAGES.maxLength} 8.`, value: 8 },
+            minLength: { message: `${ERROR_MESSAGES.minLength} 5.`, value: 5 },
+            required: { message: ERROR_MESSAGES.required, value: true },
+          }}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <>
+              <TextField
+                error={!!errors.password?.message}
+                value={value}
+                onChange={(event) => onChange(event.target.value)}
+                label="Contraseña"
+                variant="outlined"
+                type={'password'}
+                helperText={errors.password?.message}
+              />
+            </>
+          )}
         />
+        <Controller
+          control={control}
+          name="passwordConfirmation"
+          rules={{
+            maxLength: { message: `${ERROR_MESSAGES.maxLength} 8.`, value: 8 },
+            minLength: { message: `${ERROR_MESSAGES.minLength} 5.`, value: 5 },
+            required: { message: ERROR_MESSAGES.required, value: true },
+            validate: (value) =>
+              value === watch('password') || 'La contraseña no coincide.',
+          }}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <>
+              <TextField
+                error={!!errors.passwordConfirmation?.message}
+                value={value}
+                onChange={(event) => onChange(event.target.value)}
+                label="Confirmar contraseña"
+                variant="outlined"
+                type={'password'}
+                helperText={errors.passwordConfirmation?.message}
+              />
+            </>
+          )}
+        />
+
         <Typography gutterBottom variant="h8" component="div" sx={{ m: 0 }}>
           Recibir notificaciones por:
         </Typography>
         <FormGroup>
           <FormControlLabel
             control={<Checkbox defaultChecked />}
-            label="email"
+            label="Email"
           />
-          <FormControlLabel
-            control={<Checkbox defaultChecked />}
-            label="Whatsapp"
-          />
+          <FormControlLabel control={<Checkbox />} label="WhatsApp" />
         </FormGroup>
         <Stack direction="row" spacing={2}>
           <Button variant="contained">Cancelar</Button>
