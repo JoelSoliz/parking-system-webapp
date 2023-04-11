66eca53451bae56bd3e86c90adbee60be33d673d
Joel Soliz <joelsolizchoque@gmail.com>
2023-04-11 06:03:29 -0400

    feat(state): add redux dependencies. PS-39

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider as ReduxProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { CssBaseline, ThemeProvider, Typography } from '@mui/material'
 import Router from '../components/Router/Router'
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from '../theme';
-
+import theme from '../theme'
+import store, { persistor } from '../store/store'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </ThemeProvider>
-
+    <ReduxProvider store={store}>
+      <PersistGate
+        loading={<Typography>Cargando...</Typography>}
+        persistor={persistor}
+      >
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
+    </ReduxProvider>
   )
 }
-
-
 
 export default App
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+// import { persistReducer } from 'redux-persist'
+
+const rootReducer = combineReducers({})
+
+export default rootReducer
