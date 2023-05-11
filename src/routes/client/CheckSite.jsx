bfa54ccbb6b0f5dec439ceb74cf913a6ca128e30
Silvia Martinez <201704856Q04856@est.umss.edu>
2023-05-11 19:37:49 -0400

    feat(app): traer-los-datos-de-db-y-mejorar. PS-11

--- src/routes/client/CheckSite.jsx
+++ src/routes/client/CheckSite.jsx
@@ -1,167 +1,161 @@
-import React from 'react'
-import {
-  Grid,
-  Card,
-  Button,
-  TextField,
-  Typography,
-  Box,
-  Stack,
-} from '@mui/material'
+import React, { useState } from 'react'
+import { Card, Button, TextField, Typography, Box, Stack } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-import Map from '../../components/map/Map.jsx'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import esLocale from '@fullcalendar/core/locales/es.js'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
 
 const CheckSite = () => {
   const navigate = useNavigate()
-
+  const [isValid, setIsChecked] = useState(false)
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked)
+  }
   return (
-    <Box width="98%">
-      <Typography
-        gutterBottom
-        variant="h4"
-        component="div"
-        align="center"
-        marginTop={-2}
-        fontWeight="bold"
+    <Box width="55%" paddingX="60px" margin={'15px'}>
+      <Card
+        sx={{
+          paddingY: 5,
+          paddingX: '60px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 3,
+          borderRadius: '15px',
+          border: 5,
+        }}
+        style={{ borderColor: '#90b4ce' }}
       >
-        Verificar Sitio
-      </Typography>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Grid container spacing={1}>
-          <Grid item xs={10} sm={7}>
-            <Card
-              sx={{
-                width: '100%',
-                display: 'flex',
-                overflow: 'auto',
-              }}
-            >
-              <Map />
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={5}>
-            <Card
-              sx={{
-                p: 3,
-                py: 3,
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                borderRadius: '15px',
-                border: 5,
-                px: 5,
-              }}
-              style={{ borderColor: '#90b4ce' }}
-            >
-              <Box>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  align="center"
-                  fontWeight="bold"
-                >
-                  Datos de solicitud
-                </Typography>
-              </Box>
-              <TextField
-                label="Código de espacio"
-                value={'DOC117'}
-                variant="outlined"
-                disabled={true}
-              />
-              <Box display="flex" direction="row" letterSpacing={1}>
-                <TextField
-                  label="Fecha de inicio"
-                  value={'9/May/2023'}
-                  type={'text'}
-                  disabled={true}
-                  style={{ flexGrow: 1, flexShrink: 1, marginRight: '10px' }}
-                />
-                <TextField
-                  label="Fecha fin"
-                  value={'16/May/2023'}
-                  type={'text'}
-                  disabled={true}
-                  style={{ flexGrow: 1, flexShrink: 1 }}
-                />
-              </Box>
-              <Card sx={{ width: '100%' }}>
-                <FullCalendar
-                  plugins={[dayGridPlugin, timeGridPlugin]}
-                  initialView="timeGridWeek"
-                  hiddenDays={[0]}
-                  height={'auto'}
-                  contentHeight={2.5 * 50}
-                  locale={esLocale}
-                  fixedWeekCount={false}
-                  headerToolbar={{
-                    end: 'title',
-                    start:
-                      'dayGridMonth,timeGridWeek,timeGridDay prev,next today',
-                  }}
-                  navLinks={true}
-                  slotMinTime="06:30:00"
-                  slotMaxTime="22:00:00"
-                  slotDuration="1:00:00"
-                  slotLabelFormat={{
-                    hour: 'numeric',
-                    minute: '2-digit',
-                    meridiem: 'short',
-                    omitZeroMinute: false,
-                    hour12: false,
-                  }}
-                  validRange={() => ({
-                    start: new Date(
-                      new Date().getFullYear(),
-                      new Date().getMonth(),
-                      1,
-                    ),
-                    end: new Date(new Date().getFullYear(), 6, 32),
-                  })}
-                  events={[
-                    {
-                      title: 'Event 1',
-                      start: '2023-05-08T09:00:00',
-                      end: '2023-05-08T11:00:00',
-                      backgroundColor: 'green',
-                    },
-                    {
-                      title: 'Event 2',
-                      start: '2023-05-10T10:00:00',
-                      end: '2023-05-10T12:00:00',
-                      backgroundColor: 'blue',
-                    },
-                    {
-                      title: 'Event 3',
-                      start: '2023-05-11T14:00:00',
-                      end: '2023-05-11T16:00:00',
-                      backgroundColor: 'red',
-                    },
-                  ]}
-                />
-              </Card>
-              <Stack direction="row" spacing={2} justifyContent={'center'}>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => navigate('/')}
-                >
-                  Asignar
-                </Button>
-                <Button variant="contained" color="secondary">
-                  Volver
-                </Button>
-              </Stack>
-            </Card>
-          </Grid>
-        </Grid>
-      </Box>
+        <Box>
+          <Typography
+            gutterBottom
+            variant="h4"
+            component="div"
+            align="center"
+            fontWeight="bold"
+          >
+            Verificar sitio
+          </Typography>
+        </Box>
+        <TextField
+          label="Código de espacio"
+          value={'DOC117'}
+          variant="outlined"
+          disabled={true}
+        />
+        <Box display="flex" direction="row" letterSpacing={1}>
+          <TextField
+            label="Fecha de inicio"
+            value={'9/May/2023'}
+            type={'text'}
+            disabled={true}
+            style={{ flexGrow: 1, flexShrink: 1, marginRight: '10px' }}
+          />
+          <TextField
+            label="Fecha fin"
+            value={'16/May/2023'}
+            type={'text'}
+            disabled={true}
+            style={{ flexGrow: 1, flexShrink: 1 }}
+          />
+        </Box>
+        <Card sx={{ width: '100%' }}>
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin]}
+            initialView="timeGridWeek"
+            hiddenDays={[0]}
+            height={'auto'}
+            contentHeight={2.5 * 50}
+            locale={esLocale}
+            fixedWeekCount={false}
+            headerToolbar={{
+              start: 'prev,next today',
+              center: 'title',
+              end: 'dayGridMonth,timeGridWeek,timeGridDay',
+            }}
+            navLinks={true}
+            slotMinTime="06:30:00"
+            slotMaxTime="22:00:00"
+            slotDuration="1:00:00"
+            slotLabelFormat={{
+              hour: 'numeric',
+              minute: '2-digit',
+              meridiem: 'short',
+              omitZeroMinute: false,
+              hour12: false,
+            }}
+            showNonCurrentDates={false}
+            nowIndicator={true}
+            initialDate={new Date()}
+            validRange={() => ({
+              start: new Date(
+                new Date().getFullYear(),
+                new Date().getMonth(),
+                1,
+              ),
+              end: new Date(new Date().getFullYear(), 11, 32),
+            })}
+            events={[
+              {
+                title: 'Reserva 1',
+                start: '2023-05-01T09:00:00',
+                end: '2023-05-01T11:00:00',
+                backgroundColor: 'green',
+              },
+              {
+                title: 'Reserva 1',
+                start: '2023-05-08T09:00:00',
+                end: '2023-05-08T11:00:00',
+                backgroundColor: 'green',
+              },
+              {
+                title: 'Reserva 1',
+                start: '2023-05-15T09:00:00',
+                end: '2023-05-15T11:00:00',
+                backgroundColor: 'green',
+              },
+              {
+                title: 'Event 10',
+                start: '2023-06-01T10:00:00',
+                end: '2023-06-01T12:00:00',
+                backgroundColor: 'blue',
+              },
+              {
+                title: 'Event 3',
+                start: '2023-05-11T14:00:00',
+                end: '2023-05-11T16:00:00',
+                backgroundColor: 'red',
+              },
+            ]}
+          />
+        </Card>
+        <FormControlLabel
+          control={
+            <Checkbox
+              type="checkbox"
+              checked={isValid}
+              onChange={handleCheckboxChange}
+              color="primary"
+            />
+          }
+          label="El sitio está disponible los días y las horas
+              solicitadas."
+        />
+        <Stack direction="row" spacing={2} justifyContent={'center'}>
+          <Button variant="contained" color="secondary" disabled={!isValid}>
+            Asignar sitio
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => navigate('/')}
+          >
+            Volver a solicitud
+          </Button>
+        </Stack>
+      </Card>
     </Box>
   )
 }
